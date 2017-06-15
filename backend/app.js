22f7c5efb6554bd1f29b161caceeb997051f195d
THIAGO HENRIQUE SOUSA BAPTISTA <thiago.baptista@viavarejo.com.br>
2017-06-15 19:23:12 -0300

    Create 404 error handler for non existing routes

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const app = require('express')();
 
-const stats = require('./app/routes/stats');
-const urls = require('./app/routes/urls');
-const user = require('./app/routes/user');
-const users = require('./app/routes/users');
+const statsRoute = require('./app/routes/stats');
+const urlsRoute  = require('./app/routes/urls');
+const userRoute  = require('./app/routes/user');
+const usersRoute = require('./app/routes/users');
+const otherRoute = require('./app/routes/other');
 
 app.set('port', process.env.PORT || '8080');
 
-app.use('/stats', stats);
-app.use('/urls',  urls);
-app.use('/user',  user);
-app.use('/users', users);
+app.use('/stats', statsRoute);
+app.use('/urls',  urlsRoute);
+app.use('/user',  userRoute);
+app.use('/users', usersRoute);
+app.use('/*',     otherRoute);
 
 app.listen(app.get('port'));
---
+++ backend/app/routes/other.js
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+
+router.route('*').get(function(req, res) {
+
+    res.status(404).send();
+
+});
+
+module.exports = router;
+
+
