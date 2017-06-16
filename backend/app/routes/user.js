7a3dde7b704a9d81b212cd4ef7fe2472defbab94
THIAGO HENRIQUE SOUSA BAPTISTA <thiago.baptista@viavarejo.com.br>
2017-06-15 21:52:38 -0300

    Add routing handlers to /user/ path

--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -1,9 +1,19 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const httpUtils = require('../utils/http');
+const deleteUserIdPath = '/:userId';
 
-router.get('/', function(req, res, next) {
+/**
+ * Handles DELETE http requests to @deleteUserIdPath 
+ */
+router.delete(deleteUserIdPath, function(req, res, next) {
 
-  res.send('GET USER');
+  res.send('DELETE ' + deleteUserIdPath);
 
 });
+
+/**
+ * Handles any http requests to valid paths with not allowed http verbs(405)
+ */
+router.all(deleteUserIdPath, httpUtils.notAllowedHandler);
 
 module.exports = router;
