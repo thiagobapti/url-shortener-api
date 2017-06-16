1ad5674b4d1470167c7848b38174376f0632140c
THIAGO HENRIQUE SOUSA BAPTISTA <thiago.baptista@viavarejo.com.br>
2017-06-15 21:08:43 -0300

    Add routing handlers for /urls/ path

--- backend/app/routes/urls.js
+++ backend/app/routes/urls.js
@@ -1,9 +1,31 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const httpUtils = require('../utils/http');
+const getIdPath = '/:id';
+const deleteIdPath = '/:id';
 
-router.get('/', function(req, res, next) {
+/**
+ * Handles GET http requests to @getIdPath 
+ */
 
-  res.send('GET URLS');
+router.get(getIdPath, function(req, res, next) {
+
+  res.send('GET ' + getIdPath);
 
 });
+
+/**
+ * Handles DELETE http requests to @deleteIdPath 
+ */
+router.delete(deleteIdPath, function(req, res, next) {
+
+  res.send('DELETE ' + deleteIdPath);
+
+});
+
+/**
+ * Handles any http requests to valid paths with not allowed http verbs(405)
+ */
+router.all(getIdPath, httpUtils.notAllowedHandler);
+router.all(deleteIdPath, httpUtils.notAllowedHandler);
 
 module.exports = router;
