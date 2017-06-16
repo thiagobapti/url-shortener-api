444efcec57e9f6ba25610a0e90d540ee58f65628
THIAGO HENRIQUE SOUSA BAPTISTA <thiago.baptista@viavarejo.com.br>
2017-06-15 21:45:22 -0300

    Add routing handlers for /users/ path

--- backend/app/routes/users.js
+++ backend/app/routes/users.js
@@ -1,9 +1,41 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const httpUtils = require('../utils/http');
+const postRootPath = '/';
+const postUserIdUrlsPath = '/:userid/urls';
+const getUserIdStatsPath = '/:userId/stats';
 
-router.get('/', function(req, res, next) {
+/**
+ * Handles POST http requests to @postRootPath 
+ */
+router.post(postRootPath, function(req, res, next) {
 
-  res.send('GET USERS');
+  res.send('POST ' + postRootPath);
 
 });
+
+/**
+ * Handles POST http requests to @postUserIdUrlsPath 
+ */
+router.post(postUserIdUrlsPath, function(req, res, next) {
+
+  res.send('POST ' + postUserIdUrlsPath);
+
+});
+
+/**
+ * Handles GET http requests to @getUserIdStatsPath 
+ */
+router.get(getUserIdStatsPath, function(req, res, next) {
+
+  res.send('GET ' + getUserIdStatsPath);
+
+});
+
+/**
+ * Handles any http requests to valid paths with not allowed http verbs(405)
+ */
+router.all(postRootPath, httpUtils.notAllowedHandler);
+router.all(postUserIdUrlsPath, httpUtils.notAllowedHandler);
+router.all(getUserIdStatsPath, httpUtils.notAllowedHandler);
 
 module.exports = router;
