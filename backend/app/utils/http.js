e11405ea38c558f6d87b576bf3f66da0acb828c1
Thiago Baptista <thiago@syncmob.com.br>
2017-06-19 13:29:28 -0300

    Creates the general error 500 handler

--- backend/app/utils/http.js
+++ backend/app/utils/http.js
@@ -7,5 +7,18 @@ module.exports = {
 
 	  res.status(405).send();
 
+	},
+
+	/**
+ 	* Handles Firebase errors
+	 */
+    databaseErrorHandler:function(error, res) {
+
+    	var message = message || (error && error.code) || null;
+	  
+	  	if(res)
+			res.status(500).send(message);
+
 	}
+
 }
